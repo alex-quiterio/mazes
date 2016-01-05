82c80053b6c79c72ad6fdb6ed8e8720b5b276dc5
Alex Quiterio <afp.quiterio@gmail.com>
2016-01-05 23:56:50 +0000

    remove silly expectation from book spec. Rails already ensure that belongs_to does not have more than one record created. That constraint should make sense in a aux table.

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,22 +3,12 @@ require 'spec_helper'
 
 describe Book do
 
-		it { is_expected.to validate_presence_of :title }
-		it { is_expected.to validate_presence_of :author }
-	 context "#factory" do
-    it 'should be valid' do
-      expect(Fabricate(:book)).to be_valid
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :author }
+
+    context "#factory" do
+      it 'should be valid' do
+        expect(Fabricate(:book)).to be_valid
+      end
     end
-  end
-
-  context "domain" do
-   it 'should only have an author' do
-    author_1 = Fabricate(:author)
-    author_2 = Fabricate(:author)
-    book = Fabricate(:book, author: author_1)
-    author_2.books << book
-    expect(book.author).to eq(author_2)
-   end
-  end
-
 end
