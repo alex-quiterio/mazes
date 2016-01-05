7d984890cd66f4a1481573e4f637510d150ed6e9
Alex Quiterio <afp.quiterio@gmail.com>
2016-01-05 23:53:57 +0000

    update/refactor authors controller specs

--- spec/controllers/api/v1/authors_controller_spec.rb
+++ spec/controllers/api/v1/authors_controller_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 describe Api::V1::AuthorsController, type: :controller do
 
-  let(:user) { Fabricate(:user_authenticated) }
-  let(:secondary_user) { Fabricate(:user_authenticated) }
+  let(:user_1) { Fabricate(:user_authenticated) }
+  let(:user_2) { Fabricate(:user_authenticated) }
 
   describe "#index" do
-    let!(:user_authors) { 10.times.map { Fabricate(:author, user: user) } }
-    let!(:secondary_user_authors) { 5.times.map { Fabricate(:author, user: secondary_user) } }
+    let!(:user_1_authors) { 10.times.map { Fabricate(:author, user: user_1) } }
+    let!(:user_2_authors) { 5.times.map { Fabricate(:author, user: user_2) } }
 
     context "scoped by User" do
       it "should return only Authors owned by scoped User" do
-        get :index, user_id: user.id
+        get :index, user_id: user_1.id
         res = jsonfy(response.body)
         expect(response).to be_ok
-        expect(res.size).to eq(user_authors.size)
-        expect(res.map { |a| a[:id] }).to eq(user_authors.map(&:id))
+        expect(res.size).to eq(user_1_authors.size)
+        expect(res.map { |a| a[:id] }).to eq(user_1_authors.map(&:id))
 
-        get :index, user_id: secondary_user.id
+        get :index, user_id: user_2.id
         res = jsonfy(response.body)
         expect(response).to be_ok
-        expect(res.size).to eq(secondary_user_authors.size)
-        expect(res.map { |a| a[:id] }).to eq(secondary_user_authors.map(&:id))
+        expect(res.size).to eq(user_2_authors.size)
+        expect(res.map { |a| a[:id] }).to eq(user_2_authors.map(&:id))
       end
     end
 
@@ -30,7 +30,7 @@ describe Api::V1::AuthorsController, type: :controller do
         get :index
         res = jsonfy(response.body)
         expect(response).to be_ok
-        expect(res.size).to eq(user_authors.size+secondary_user_authors.size)
+        expect(res.size).to eq(user_1_authors.size+user_2_authors.size)
       end
     end
   end
@@ -50,40 +50,40 @@ describe Api::V1::AuthorsController, type: :controller do
       let(:author_params) { Fabricate.attributes_for(:author) }
 
       it "should return the created Author" do
-        post :create, author: author_params, token: user.remember_token
+        post :create, author: author_params, token: user_1.remember_token
         res = jsonfy(response.body)
         expect(response).to be_ok
         expect(res[:name]).to eq(author_params[:name])
         expect(res[:nationality]).to eq(author_params[:nationality])
-        expect(res[:user_id]).to eq(user.id)
+        expect(res[:user_id]).to eq(user_1.id)
       end
     end
   end
 
   describe "#update" do
-    let(:author) { Fabricate(:author, user: user) }
+    let(:author) { Fabricate(:author, user: user_1) }
     let(:new_author_params) { { nationality: "french", name: 'Albert Camus' } }
 
     context "User w/ normal role" do
       it "should be able to update his Authors successfully" do
-        patch :update, id: author.id, author: new_author_params, token: user.remember_token
+        patch :update, id: author.id, author: new_author_params, token: user_1.remember_token
         res = jsonfy(response.body)
         expect(response).to be_ok
         expect(res[:id]).to eq(author.id)
         expect(res[:name]).to eq(new_author_params[:name])
         expect(res[:nationality]).to eq(new_author_params[:nationality])
-        expect(res[:user_id]).to eq(user.id)
+        expect(res[:user_id]).to eq(user_1.id)
       end
 
       it "should not be able to update Authors from other Users" do
-        patch :update, id: author.id, author: new_author_params, token: secondary_user.remember_token
+        patch :update, id: author.id, author: new_author_params, token: user_2.remember_token
         res = jsonfy(response.body)
         expect(response).to be_unauthorized
         expect(res[:errors].first).to include('Not allowed to perform this action')
       end
 
       it "should not be able to update an Author with invalid params" do
-        patch :update, id: author.id, author: { name: nil }, token: user.remember_token
+        patch :update, id: author.id, author: { name: nil }, token: user_1.remember_token
         res = jsonfy(response.body)
         expect(response).to be_unprocessable
         expect(res[:errors].first).to include("can't be blank")
@@ -100,25 +100,25 @@ describe Api::V1::AuthorsController, type: :controller do
         expect(res[:id]).to eq(author.id)
         expect(res[:name]).to eq(new_author_params[:name])
         expect(res[:nationality]).to eq(new_author_params[:nationality])
-        expect(res[:user_id]).to eq(user.id)
+        expect(res[:user_id]).to eq(user_1.id)
       end
     end
   end
 
   describe "#destroy" do
 
-    let(:author) { Fabricate(:author, user: user) }
+    let(:author) { Fabricate(:author, user: user_1) }
 
     context "User w/ normal role" do
       it "should be able a User to delete his Authors" do
         author_id = author.id
-        delete :destroy, id: author_id, token: user.remember_token
+        delete :destroy, id: author_id, token: user_1.remember_token
         expect { Author.find(author_id) }.to raise_error(ActiveRecord::RecordNotFound)
         expect(response).to be_ok
       end
 
       it "should not be able to delete Authors from other Users" do
-        delete :destroy, id: author.id, token: secondary_user.remember_token
+        delete :destroy, id: author.id, token: user_2.remember_token
         res = jsonfy(response.body)
         expect(res[:errors].first).to include('Not allowed to perform this action')
         expect(response).to be_unauthorized
